d31a3f81520277a3088374802a2d43e886330135
Lcisnerosm <53229664+Lcisnerosm@users.noreply.github.com>
2020-11-18 03:06:01 -0600

    sales

--- src/app/models/customer.model.js
+++ src/app/models/customer.model.js
@@ -50,12 +50,13 @@ customStorage.getbybranch = async (br) => {
 }
 
 customStorage.getproductbycustomer = async (pr) => {
-    var sql = `select sa.idsales as factura, sa.date, pr.name,de.price, de.quantity, de.total
-                from sales sa
-                inner join datails de on de.iddatails = sa.idsales
-                inner join product pr on pr.idproduct = de.product_idproduct
-                inner join customers cu on cu.idcustomer = sa.customer_idcustomer
-                where cu.idcustomer = ?;`
+    var sql = `select da.iddatails as factura, c.name as name_customer, 
+    s.date as fecha, da.price, da.quantity, da.total,
+     p.name as name_product from datails da
+    inner join sales s on da.sales_idsales = s.idsales
+    inner join product p on da.product_idproduct = p.idproduct
+    left join customers c on s.customer_idcustomer = c.idcustomer
+    where c.idcustomer = ?`
     return new Promise((resolve, reject) => {
         connect.query(sql, [pr], (err, rows) => {
             if (err) {
